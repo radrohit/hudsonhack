f2289f46b6b2ee1025e82af83de2c8e35b0391f5
Rohith <radrohit@gmail.com>
2015-07-18 19:24:35 -0400

    Get 1000 entries from the data

--- test.py
+++ test.py
@@ -5,20 +5,24 @@ import urllib
 import json
 
 #url data
-url_string = "http://api.donorschoose.org/common/json_feed.html?state=%s&community=%s&APIKey=DONORSCHOOSE" % ("NC","10007:3")
+#url_string = "http://api.donorschoose.org/common/json_feed.html?state=%s&community=%s&APIKey=DONORSCHOOSE" % ("NC","10007:3")
 main_url = "http://api.donorschoose.org/common/json_feed.html"
-values ={
-         'APIKey': "DONORSCHOOSE",
-         'max':'50',
-         'index': '0'
-}
+def get_data(main_url,values):
+    data= urllib.urlencode(values)
+    request = Request(main_url,data)
+    response = urlopen(request)
+    website_data = json.loads(response.read())
+    print "working"
+    return website_data
 
-
-# getting the json value
-data= urllib.urlencode(values)
-request = Request(main_url,data)
-response = urlopen(request)
-website_data = json.loads(response.read())
-
-for proposal in website_data['proposals']:
-    print proposal['fulfillmentTrailer']
+max_value = 20
+proposals = []
+for i in range(max_value):
+    values ={
+             'APIKey': "DONORSCHOOSE",
+             'max':'50',
+             'index': str(i*50 + 1)
+    }
+    website_data = get_data(main_url,values)
+    for proposal in website_data['proposals']:
+        proposals.append(proposal['fulfillmentTrailer'])
