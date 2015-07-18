65e15bbf125036a77d4c03670d7f316b221429f2
Rohith <radrohit@gmail.com>
2015-07-18 18:56:47 -0400

    Added little bit more functionality

--- test.py
+++ test.py
@@ -2,17 +2,23 @@
 #importing modules
 from urllib2 import Request, urlopen, URLError
 import urllib
+import json
 
 #url data
 url_string = "http://api.donorschoose.org/common/json_feed.html?state=%s&community=%s&APIKey=DONORSCHOOSE" % ("NC","10007:3")
 main_url = "http://api.donorschoose.org/common/json_feed.html"
-values ={'state': 'NC',
-         'community': "10007:3",
-         'APIKey': "DONORSCHOOSE"
+values ={
+         'APIKey': "DONORSCHOOSE",
+         'max':'50',
+         'index': '0'
 }
+
 
 # getting the json value
 data= urllib.urlencode(values)
 request = Request(main_url,data)
 response = urlopen(request)
-print response.read()
+website_data = json.loads(response.read())
+
+for proposal in website_data['proposals']:
+    print proposal['fulfillmentTrailer']
